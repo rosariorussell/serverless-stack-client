a5d7ba79f9af49498935810761ef6548db2fd707
Russell Rosario <rosariorussell@gmail.com>
2018-11-09 18:44:02 -0500

    updated serverless api connections

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_8rGGJh99hpCamR3vhDGLbfce",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1491po0syjmnh"
+    BUCKET: "topcofounder-tasks-dev-attachmentsbucket-nzfq609zq1y7"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://ia50qu1i3g.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://mu2005o9xb.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_Zut33hUck",
-    APP_CLIENT_ID: "73sb1fvm76istsb19qtcpimj03",
-    IDENTITY_POOL_ID: "us-east-1:350b66d5-ed75-49d2-b955-804027d5d76a"
+    USER_POOL_ID: "us-east-1_yOIrRfyo3",
+    APP_CLIENT_ID: "2frrv98aj87rqeh3u8026q3uh4",
+    IDENTITY_POOL_ID: "us-east-1:4281ced2-9050-4c5c-b410-7ff8b0c5c1a0"
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: "pk_test_8rGGJh99hpCamR3vhDGLbfce",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-9vhmj1ie0h60"
+    BUCKET: "topcofounder-tasks-prod-attachmentsbucket-lxvmnjhemw8v"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://cj5484pq64.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://jqzdlq9p87.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_b9DG0MIPa",
-    APP_CLIENT_ID: "1mnfs5jsio3ntrgmosl1vpn8ul",
-    IDENTITY_POOL_ID: "us-east-1:848ff18d-5840-40a6-9f10-532ec8392e26"
+    USER_POOL_ID: "us-east-1_eTquHPWkG",
+    APP_CLIENT_ID: "ec6b3h80nchah7a1f5vufb0bf",
+    IDENTITY_POOL_ID: "us-east-1:e68fc2ca-12ff-461f-82ea-26d349ee3d69"
   }
 };
 
