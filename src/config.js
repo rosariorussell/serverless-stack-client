fe92ce4c14a4c177d7ea2c3609a0469e1622c223
Russell Rosario <rosariorussell@gmail.com>
2018-11-10 02:23:26 -0500

    fixed dynamoDB schema

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_8rGGJh99hpCamR3vhDGLbfce",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "topcofounder-tasks-dev-attachmentsbucket-s6ezndyk96qi"
+    BUCKET: "topcofounder-tasks-v2-dev-attachmentsbucket-1pblz4pk9q6c6"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://6u94edswr3.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://3lcj7iy1i0.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_rLuZ0MEDF",
-    APP_CLIENT_ID: "k8t2qjpsvkclqanhn52uuvlln",
-    IDENTITY_POOL_ID: "us-east-1:e58f0b41-5e78-487c-a944-afe95a2b607d"
+    USER_POOL_ID: "us-east-1_Y0HeGrFHO",
+    APP_CLIENT_ID: "7prq4o9emvl603uv7c4duephbt",
+    IDENTITY_POOL_ID: "us-east-1:5eeb4fac-bb6b-4f03-9d90-6d0952fd6282"
   }
 };
 
@@ -20,17 +20,17 @@ const prod = {
   STRIPE_KEY: "pk_test_8rGGJh99hpCamR3vhDGLbfce",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "topcofounder-tasks-prod-attachmentsbucket-1hy4hgfc7ifib"
+    BUCKET: "topcofounder-tasks-v2-prod-attachmentsbucket-yovry1nryqhd"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://atkeza7ok4.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://pnhym59sp0.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_XZPWLYHIn",
-    APP_CLIENT_ID: "41nllc41p3gofr8lum3fthea1r",
-    IDENTITY_POOL_ID: "us-east-1:3c51a3f2-b5a3-4dd7-9dc0-63a6e154302a"
+    USER_POOL_ID: "us-east-1_xDwhDxCW7",
+    APP_CLIENT_ID: "65ejcion7c7nhnb7l0abilprjb",
+    IDENTITY_POOL_ID: "us-east-1:53c522e2-19b9-43a0-8e5e-927e6e845793"
   }
 };
 
